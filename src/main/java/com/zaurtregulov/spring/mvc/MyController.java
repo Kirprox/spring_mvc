4fdc0a210b716be567dc6f3d9c0e79fb0d9b6816
Kir <kirpro95@yandex.ru>
2025-05-26 10:03:58 +0300

    @RequestMapping for controller

--- src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
 
     @RequestMapping("/")
@@ -19,20 +20,6 @@ public class MyController {
     public String askEmployeeDetails() {
         return "ask-emp-details-view";
     }
-//    @RequestMapping("showDetails")
-//    public String showEmpDetails(){
-//        return "show-emp-details-view";
-//    }
-
-//    @RequestMapping("/showDetails")
-//    public String showEmpDetails(HttpServletRequest request, Model model) {
-//        String empName = request.getParameter("employeeName");
-//        empName = "mr. " + empName;
-//        model.addAttribute("nameAttribute",empName);
-//        model.addAttribute("description","-  udemy instructor");
-//
-//        return "show-emp-details-view";
-//    }
 
     @RequestMapping("/showDetails")
     public String showEmpDetails(@RequestParam("employeeName") String empName, Model model) {
